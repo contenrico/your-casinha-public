e3e52bf9a6747c82dd193e265284580c01f1dc92
contenrico <contenrico94@gmail.com>
2024-04-01 22:09:45 +0100

    Selenium working in jupyter notebook

--- pages/1_🌍_Register_Guests.py
+++ pages/1_🌍_Register_Guests.py
@@ -15,9 +15,15 @@ if not st.session_state.authenticated:
 st.set_page_config(page_title="Register Guests", page_icon="🌍")
 st.title('Register New Guests')
 
-# Initialize session state for the DataFrame
+# Initialize session state variables
 if 'filtered_df' not in st.session_state:
     st.session_state.filtered_df = pd.DataFrame()
+if 'update_message' not in st.session_state:
+    st.session_state.update_message = "Click the button to start the process..."
+if 'sef_completed' not in st.session_state:
+    st.session_state.sef_completed = False
+
+### --- GET AND DISPLAY FORM RESPONSES --- ###
 
 # Add input field with today's date by default
 checkin_date = st.date_input('Check-in date: ', value=pd.to_datetime("today"), format="DD-MM-YYYY")
@@ -39,12 +45,40 @@ if st.button('Get form responses'):
 # Editable DataFrame
 st.session_state.filtered_df = st.data_editor(st.session_state.filtered_df, hide_index=True)
 
-if st.button('Register guests on SEF'): 
-    #TODO validate check-in and check-out dates, specifically if check-in is after today, can't click
-    # Run SEF automation
-    message = web_automation.fill_in_sef_form(st.session_state.filtered_df)
+### --- REGISTER GUESTS ON SEF --- ###
 
-    # Write records to json if successful
-    if message == 'SEF Completed':
-        data_processing.write_new_records_to_json(st.session_state.filtered_df)
+# Define a callback function to update the UI and track completion
+def update_ui(message):
+    st.session_state.update_message = message
+    # Check if the process has completed successfully
+    if message == "SEF registration completed successfully.":
+        st.session_state.sef_completed = True
+    else:
+        st.session_state.sef_completed = False
+
+# if st.button('Register guests on SEF'): 
+#     #TODO validate check-in and check-out dates, specifically if check-in is after today, can't click
+#     # Run SEF automation
+#     message = web_automation.fill_in_sef_form(st.session_state.filtered_df)
+
+#     # Write records to json if successful
+#     if message == 'SEF Completed':
+#         data_processing.write_new_records_to_json(st.session_state.filtered_df)
+#         st.success('Guests registered successfully!')
+
+# Displaying the update message dynamically
+message_placeholder = st.empty()
+message_placeholder.text(st.session_state.update_message)
+
+# TODO: TEST FUNCTION ALONE AS IT'S NOT WORKING 
+if st.button('Register guests on SEF'): #TODO validate check-in and check-out dates, specifically if check-in is after today, can't click
+    # Reset completion state
+    st.session_state.sef_completed = False
+    # Run SEF automation with the adapted callback
+    web_automation.fill_in_sef_form(df=st.session_state.filtered_df, callback=update_ui)
+    # After completion, check if it was successful
+    if st.session_state.sef_completed:
+        # data_processing.write_new_records_to_json(st.session_state.filtered_df)
         st.success('Guests registered successfully!')
+    else:
+        st.error('SEF registration failed.')
--- scripts/web_automation.py
+++ scripts/web_automation.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,24 +22,151 @@ pdf_senha = os.getenv('PDF_SENHA')
 countries_mapping = os.path.join(os.getcwd(), 'parameters', 'countries_mapping.json')
 nationalities_mapping = os.path.join(os.getcwd(), 'parameters', 'nationalities_mapping.json')
 
-def fill_in_sef_form(df):
+# def fill_in_sef_form(df):
 
+#     try:
+
+#         with open(countries_mapping) as f:
+#             countries = json.load(f)
+
+#         with open(nationalities_mapping) as f:
+#             nationalities = json.load(f)
+
+#         web = webdriver.Chrome()
+
+#         url = r'https://siba.sef.pt/s/FB.aspx?ReturnUrl=%2fs%2fbal%2fLotesEnvio.aspx'
+#         web.get(url)
+
+#         time.sleep(2)
+
+#         # Find and fill in the input fields
+#         uh_input = web.find_element('id', 'Conteudo_txtUH')
+#         estabelecimento_input = web.find_element('id', 'Conteudo_txtEstabelecimento')
+#         chave_activacao_input = web.find_element('id', 'Conteudo_txtChaveActivacao')
+
+#         uh_input.send_keys(uh_input_)
+#         estabelecimento_input.send_keys(estabelecimento_input_)
+#         chave_activacao_input.send_keys(chave_activacao_input_)
+
+#         # Locate and click the submit button
+#         submit_button = web.find_element('id', 'Conteudo_btnConfirmar')
+#         submit_button.click()
+
+#         time.sleep(2)
+
+#         # Create new list - NOTE: no need to comment this out as website won't allow you to create new list if one already exists
+#         # Code will then run normally after time.sleep(2)
+#         new_list = web.find_element('id', 'Conteudo_btnNovaLista')
+#         new_list.click()
+
+#         time.sleep(2)
+
+#         # Edit new list
+#         edit_list = web.find_element('id', 'Conteudo_dg_btnSelect_0')
+#         edit_list.click()
+
+#         time.sleep(2)
+
+#         # Edit form
+#         try:
+#             edit_form = web.find_element('id', 'Conteudo_btnNovaBAL')
+#             edit_form.click()
+#         except:
+#             # Click on save first
+#             save = web.find_element('id', 'Conteudo_btnActualizarBAL')
+#             save.click()
+#             time.sleep(1)
+#             edit_form = web.find_element('id', 'Conteudo_btnNovaBAL')
+#             edit_form.click()
+
+#         time.sleep(2)
+
+#         ### START OF THE LOOP
+#         for idx, row in df.iterrows():
+
+#             # Get all fields
+#             name_ = web.find_element('id', 'Conteudo_txtNome')
+#             dob_ = web.find_element('id', 'Conteudo_txtDataNascimento')
+#             nationality_ = web.find_element('id', 'Conteudo_lstNacionalidade')
+#             country_of_res_ = web.find_element('id', 'Conteudo_lstPaisResidencia')
+#             passport_ = web.find_element('id', 'Conteudo_txtNumPassaporteBI')
+#             country_of_issue_ = web.find_element('id', 'Conteudo_lstPaisEmissor')
+#             checkin_date_ = web.find_element('id', 'Conteudo_txtDataEntrada')
+#             checkout_date_ = web.find_element('id', 'Conteudo_txtDataSaida')
+
+#             # Write in fields
+#             name = row['First name'] + ' ' + row['Last name']
+#             name_.send_keys(name)
+
+#             dob = row['Date of birth']
+#             dob_.send_keys(dob)
+
+#             nationality = countries[nationalities[row['Nationality']]]
+#             nationality_.send_keys(nationality)
+
+#             country_of_res = countries[row['Country of residence']]
+#             country_of_res_.send_keys(country_of_res)
+
+#             passport = row['Passport (or ID) number']
+#             passport_.send_keys(passport)
+
+#             country_of_issue = countries[row['Country of issue']]
+#             country_of_issue_.send_keys(country_of_issue)
+
+#             checkin_date = row['Check-in date']
+#             checkin_date_.send_keys(checkin_date)
+
+#             checkout_date = row['Check-out date']
+#             checkout_date_.send_keys(checkout_date)
+
+#             # Click on save
+#             save = web.find_element('id', 'Conteudo_btnActualizarBAL')
+#             save.click()
+
+#             print('Details saved, going to next one')
+
+#             time.sleep(3)
+
+#         ### END OF THE LOOP
+
+#         # Send the boletim
+#         send = web.find_element('id', 'Conteudo_btnEnviarLista')
+#         send.click()
+
+#         time.sleep(10)
+
+#         message = 'SEF Completed'
+
+#         print(message)
+
+#     except Exception as e:
+
+#         print(f"Error: {e}")
+#         message = e
+    
+#     return message
+
+def fill_in_sef_form(df, callback):
     try:
-
         with open(countries_mapping) as f:
             countries = json.load(f)
 
         with open(nationalities_mapping) as f:
             nationalities = json.load(f)
 
-        web = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
-        url = r'https://siba.sef.pt/s/FB.aspx?ReturnUrl=%2fs%2fbal%2fLotesEnvio.aspx'
+        web = webdriver.Chrome(options=chrome_options)
+
+        callback("Opening the SEF website...")
+        url = 'https://siba.sef.pt/s/FB.aspx?ReturnUrl=%2fs%2fbal%2fLotesEnvio.aspx'
         web.get(url)
-
         time.sleep(2)
 
-        # Find and fill in the input fields
+        callback("Filling in the establishment details...")
         uh_input = web.find_element('id', 'Conteudo_txtUH')
         estabelecimento_input = web.find_element('id', 'Conteudo_txtEstabelecimento')
         chave_activacao_input = web.find_element('id', 'Conteudo_txtChaveActivacao')
@@ -47,43 +175,22 @@ def fill_in_sef_form(df):
         estabelecimento_input.send_keys(estabelecimento_input_)
         chave_activacao_input.send_keys(chave_activacao_input_)
 
-        # Locate and click the submit button
         submit_button = web.find_element('id', 'Conteudo_btnConfirmar')
         submit_button.click()
-
         time.sleep(2)
 
-        # Create new list - NOTE: no need to comment this out as website won't allow you to create new list if one already exists
-        # Code will then run normally after time.sleep(2)
+        callback("Creating a new list...")
         new_list = web.find_element('id', 'Conteudo_btnNovaLista')
         new_list.click()
-
         time.sleep(2)
 
-        # Edit new list
+        callback("Editing the new list...")
         edit_list = web.find_element('id', 'Conteudo_dg_btnSelect_0')
         edit_list.click()
-
         time.sleep(2)
 
-        # Edit form
-        try:
-            edit_form = web.find_element('id', 'Conteudo_btnNovaBAL')
-            edit_form.click()
-        except:
-            # Click on save first
-            save = web.find_element('id', 'Conteudo_btnActualizarBAL')
-            save.click()
-            time.sleep(1)
-            edit_form = web.find_element('id', 'Conteudo_btnNovaBAL')
-            edit_form.click()
-
-        time.sleep(2)
-
-        ### START OF THE LOOP
+        callback("Preparing to fill in guest details...")
         for idx, row in df.iterrows():
-
-            # Get all fields
             name_ = web.find_element('id', 'Conteudo_txtNome')
             dob_ = web.find_element('id', 'Conteudo_txtDataNascimento')
             nationality_ = web.find_element('id', 'Conteudo_lstNacionalidade')
@@ -93,57 +200,41 @@ def fill_in_sef_form(df):
             checkin_date_ = web.find_element('id', 'Conteudo_txtDataEntrada')
             checkout_date_ = web.find_element('id', 'Conteudo_txtDataSaida')
 
-            # Write in fields
             name = row['First name'] + ' ' + row['Last name']
-            name_.send_keys(name)
-
             dob = row['Date of birth']
-            dob_.send_keys(dob)
-
             nationality = countries[nationalities[row['Nationality']]]
-            nationality_.send_keys(nationality)
-
             country_of_res = countries[row['Country of residence']]
-            country_of_res_.send_keys(country_of_res)
-
             passport = row['Passport (or ID) number']
-            passport_.send_keys(passport)
-
             country_of_issue = countries[row['Country of issue']]
-            country_of_issue_.send_keys(country_of_issue)
-
             checkin_date = row['Check-in date']
-            checkin_date_.send_keys(checkin_date)
-
             checkout_date = row['Check-out date']
+
+            name_.send_keys(name)
+            dob_.send_keys(dob)
+            nationality_.send_keys(nationality)
+            country_of_res_.send_keys(country_of_res)
+            passport_.send_keys(passport)
+            country_of_issue_.send_keys(country_of_issue)
+            checkin_date_.send_keys(checkin_date)
             checkout_date_.send_keys(checkout_date)
 
-            # Click on save
             save = web.find_element('id', 'Conteudo_btnActualizarBAL')
             save.click()
-
-            print('Details saved, going to next one')
-
+            callback(f"Details for {name} saved. Proceeding to the next guest...")
             time.sleep(3)
 
-        ### END OF THE LOOP
+        callback("Finalizing and sending the list...")
+        # send = web.find_element('id', 'Conteudo_btnEnviarLista')
+        # send.click()
+        time.sleep(5)
 
-        # Send the boletim
-        send = web.find_element('id', 'Conteudo_btnEnviarLista')
-        send.click()
-
-        time.sleep(10)
-
-        message = 'SEF Completed'
-
-        print(message)
+        callback("SEF registration completed successfully.")
 
     except Exception as e:
+        callback(f"Error during SEF registration: {e}")
 
-        print(f"Error: {e}")
-        message = e
-    
-    return message
+    finally:
+        web.quit()
 
 
 def fill_in_invoice(filtered_df, amount, date=None, invoice_nif=None):
